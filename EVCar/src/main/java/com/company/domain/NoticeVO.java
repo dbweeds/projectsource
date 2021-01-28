a57596549818aeb60e3cb1e49fa5e5949373b9cc
hyeonju <guswn8557@gmail.com>
2021-01-28 18:19:44 +0900

    공지사항 VO & DB 설계

---
+++ EVCar/src/main/java/com/company/domain/NoticeVO.java
@@ -0,0 +1,16 @@
+package com.company.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class NoticeVO {
+	private int bno;
+	private String title;
+	private String writer;
+	private String content;
+	private Date regDate;
+	private Date updateDate;
+	private int readCnt;
+}
