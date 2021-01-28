3f46e34c20826d52f70b2f39cf3d9705dabb8f97
JinHyeonJU <73865691+JinHyeonJU@users.noreply.github.com>
2021-01-28 16:36:52 +0900

    notice_board.sql create

---
+++ EVCar/src/main/webapp/WEB-INF/views/comunity/notice_board.sql
@@ -0,0 +1,14 @@
+create sequence seq_board;
+
+create table notice_board (
+	bno NUMBER(10,0),
+	title VARCHAR2(100) not null,
+	writer VARCHAR2(30) not null,
+	regDate DATE default sysdate,
+	updateDate DATE default sysdate,
+	readCnt NUMBER(10,0)
+);
+	
+alter table notice_board add constraint pk_board primary key(bno);
+
+--더미값
