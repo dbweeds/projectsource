0bd74a77a0d89fee43578adc50c30c081a75d253
hyeonju <guswn8557@gmail.com>
2021-01-29 15:29:39 +0900

    notice update

--- EVCar/src/main/java/com/company/mapper/NoticeMapper.java
+++ EVCar/src/main/java/com/company/mapper/NoticeMapper.java
@@ -1,5 +1,21 @@
 package com.company.mapper;
 
-public class NoticeMapper {
+import java.util.List;
 
+import com.company.domain.NoticeVO;
+
+public interface NoticeMapper {
+
+	//전체 조회
+	public List<NoticeVO> getList();
+	
+	public void insert(NoticeVO notice);
+	
+	public void insertSelectKey(NoticeVO notice);
+	
+	public NoticeVO read(int bno);
+	
+	public int delete(int bno);
+	
+	public int update(NoticeVO notice);
 }
