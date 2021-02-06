4cbf7f5141440fc43906f78443fdf5be5a89bb33
seosooseong <tjtntjd82@naver.com>
2021-02-06 14:25:41 +0900

    info목록,상세페이지,사진들

--- EVCar/src/main/java/com/company/controller/InfoController.java
+++ EVCar/src/main/java/com/company/controller/InfoController.java
@@ -1,10 +1,21 @@
 package com.company.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.company.domain.CarInfoVO;
+import com.company.service.InfoService;
 
 import lombok.extern.slf4j.Slf4j;
+import oracle.jdbc.proxy.annotation.Post;
 
 //자동차 소개 컨트롤러
 @Controller
@@ -12,8 +23,48 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/info/*")
 public class InfoController {
 	
-	@GetMapping("/test")
-	public void getest() {
-		log.info("test시작");
+	@Autowired
+	private InfoService service;
+	
+	//등록 폼
+	@GetMapping("/infoRegist")
+	public void regist() {
+		log.info("등록 폼 OPEN...");
 	}
+	
+	//작성
+	@PostMapping("/infoRegist")
+	public String registPost(CarInfoVO info){
+		log.info("차량 등록..."+info);
+		service.regist(info);
+		return "redirect:list";
+	}
+	
+	//목록
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("전기차 목록...");
+		List<CarInfoVO> list = service.getList();
+		model.addAttribute("list",list);
+
+	}
+	/*
+	 * @GetMapping("/list2") public void list2(Model model) { log.info("전기차 목록...");
+	 * List<CarInfoVO> list = service.getList(); model.addAttribute("list",list);
+	 * 
+	 * }
+	 */
+	
+	@GetMapping("/inforead")
+	public void get(int bno,Model model) {
+		log.info("차량 상세페이지..."+bno);
+		CarInfoVO info=service.getRow(bno);
+		model.addAttribute("info", info);
+	}
+	
+	
+	//검색 ..모르겠다;;
+	
+	
+	//수정
 }
---
+++ EVCar/src/main/java/com/company/mapper/InfoMapper.java
@@ -0,0 +1,15 @@
+package com.company.mapper;
+
+import java.util.List;
+
+import com.company.domain.CarInfoVO;
+
+public interface InfoMapper {
+	
+	public int insert(CarInfoVO info); //삽입
+	public int delete(int bno); //삭제
+	public int update(CarInfoVO info); //제목 내용
+	public List<CarInfoVO> list(); //리스트
+	public CarInfoVO read(int bno); //상세페이지
+	//검색
+}
